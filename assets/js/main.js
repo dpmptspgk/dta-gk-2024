a66a8b16d4956171f5e5889151f35164c42485cc
Tyo <nurhandipa632@gmail.com>
2024-05-07 22:30:14 +0700

    feat: adjust layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,2 @@
-// navbar border bottom on scroll
-let navscroll = document.querySelector(".navbar-scroll")
-
-window.onscroll = function () {
-  if (window.pageYOffset > 0) {
-    navscroll.classList.add("scrolled")
-  } else {
-    navscroll.classList.remove("scrolled")
-  }
-}
-
 // animate on scroll
 AOS.init();
